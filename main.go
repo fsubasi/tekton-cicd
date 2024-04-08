b443e2db05efc9e3e0765b5172dd59854b858258
Feridun Subasi <frdnbrcn@gmail.com>
2024-04-08 16:55:25 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,5 +8,5 @@ func main() {
     http.ListenAndServe(":8080", nil)
 }
 func HelloWorld(w http.ResponseWriter, r * http.Request) {
-    fmt.Fprintf(w, "Hello World!")
+    fmt.Fprintf(w, "Hello World")
 }
